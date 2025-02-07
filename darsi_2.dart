008f7c6bba2a7fc092f2c795d445051350caddf3
Temiraliyev <156788797+temiraliyevm@users.noreply.github.com>
2025-02-07 12:24:46 +0500

    Дарси 2

---
+++ darsi_2.dart
@@ -0,0 +1,22 @@
+void main() {
+  // Tағйирёбандаи int
+
+  int sinnu_sol = 12;
+  int soli_tavallud = 2003;
+
+  print(sinnu_sol);
+  print(soli_tavallud);
+
+  //Хусусиятҳои int
+
+  int butun = 10;
+  // int dahi = 10.5; // хато
+
+  // Мисолҳо
+  int adadi1 = 12;
+  int adadi2 = 13;
+  print(adadi1 + adadi2); // жамъ
+  print(adadi1 - adadi2); // тарх
+  print(adadi1 * adadi2); // зарб
+  print(adadi1 / adadi2); // таксим
+}
