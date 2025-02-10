68644af22fe5efbee7818d27fd8bd37580bb5cbf
Temiraliyev <156788797+temiraliyevm@users.noreply.github.com>
2025-02-10 12:11:33 +0500

    Дарси 4 ва Вазифаи хонагии дарси 3

--- darsi_3.dart
+++ darsi_3.dart
@@ -1,3 +1,5 @@
+// Дарси-3. Tағйирёбандаи double
+
 void main() {
   // double narx = 25.5;
   // print(narx);
@@ -12,13 +14,13 @@ void main() {
 
   // Амалҳои арифметикӣ бо double
 
-  double a = 10.2;
-  double b = 4.5;
+  // double a = 10.2;
+  // double b = 4.5;
 
-  print(a + b);
-  print(a - b);
-  print(a * b);
-  print(a / b);
+  // print(a + b);
+  // print(a - b);
+  // print(a * b);
+  // print(a / b);
 
   //----------------------------------------------------------------------------
 
@@ -29,7 +31,31 @@ void main() {
   // нархи маҳсулот, ҳарорати ҳаво, вазни шахс…).
   // - Онҳоро дар `console` чоп кунед.
 
-  // 1. **Истифодаи амалҳои арифметикӣ**
+  // double narxiMahsulot = 15.99;
+  // double harorat = -2.5;
+  // double vazn = 70.5;
+
+  // // Чоп кардани арзишҳо
+  // print("Нархи маҳсулот: \$${narxiMahsulot}");
+  // print("Ҳарорати ҳаво: ${harorat}°C");
+  // print("Вазни шахс: ${vazn} кг");
+
+  // 2. **Истифодаи амалҳои арифметикӣ**
   // - Ду адади `double` гиред ва онҳоро ҷамъ, тарҳ, зарб, тақсим кунед.
   // - Натиҷаҳоро дар `console` чоп кунед.
+
+  double a = 12.5;
+  double b = 3.2;
+
+  double jam = a + b;
+  double tarh = a - b;
+  double zarb = a * b;
+  double taqsim = a / b;
+
+  // Чоп кардани натиҷаҳо
+  print("\nАмалҳои арифметикӣ:");
+  print("$a + $b = $jam");
+  print("$a - $b = $tarh");
+  print("$a * $b = $zarb");
+  print("$a / $b = $taqsim");
 }
---
+++ darsi_4.dart
@@ -0,0 +1,12 @@
+void main() {
+  // Усули оддӣ (List-и оддӣ)
+  // List ruyxat = ["Mujbali", "Temiraliyev", 2003];
+  // print(ruyxat);
+
+  // Эълон кардани List-и холӣ ва илова кардани элементҳо
+  List raqamho = [];
+
+  raqamho.add(10);
+  raqamho.add("Raqam");
+  print(raqamho);
+}
